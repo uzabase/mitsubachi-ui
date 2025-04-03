b33cb8ee1c0cce1dfa7552c01390b29d74f41605
Ryotaro <ryotaro612.ooo@gmail.com>
2025-04-03 20:24:06 +0900

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 export { SpButton } from "./components/button/sp-button";
 export { SpIcon } from "./components/icon";
 export { SpLabelUnit } from "./components/label-unit";
+export { SpLogo } from "./components/logo";
 export { SpTextFieldUnit } from "./components/text-field/text-field-unit";
