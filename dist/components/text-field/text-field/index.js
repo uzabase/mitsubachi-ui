2868dde899bce2a2fa3666aee032ab7eea949c17
Ryotaro <ryotaro612.ooo@gmail.com>
2025-04-08 19:28:52 +0900

    1

--- dist/components/text-field/text-field/index.js
+++ dist/components/text-field/text-field/index.js
@@ -64,6 +64,7 @@ export class SpTextField extends HTMLElement {
     }
     set value(value) {
         __classPrivateFieldGet(this, _SpTextField_input, "f").value = value;
+        console.log('called', __classPrivateFieldGet(this, _SpTextField_input, "f").value, this.value);
         __classPrivateFieldGet(this, _SpTextField_internals, "f").setFormValue(this.value);
         __classPrivateFieldGet(this, _SpTextField_instances, "m", _SpTextField_updateAttribute).call(this, "value", value);
     }
