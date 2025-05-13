38178b3c7488b0b1c67416748d2ad17e70921f7a
Ryotaro <ryotaro612.ooo@gmail.com>
2025-05-13 14:15:17 +0900

    Add new icons: chevronDown, chevronDownSmall, and globe; update documentation and exports

--- dist/components/icon/index.d.ts
+++ dist/components/icon/index.d.ts
@@ -3,7 +3,7 @@
  *
  * @summary アイコンです。
  *
- * @attr {string} type - iconの画像を定義します。error-fillは赤いバツ印。information-circleは逆向きの!マーク。personは肩より上の人のアイコンです。checkCircleは白い丸の中にチェックマークがあります。
+ * @attr {string} type - iconの画像を定義します。error-fillは赤いバツ印。information-circleは逆向きの!マーク。personは肩より上の人のアイコンです。checkCircleは白い丸の中にチェックマークがあります。  chevronDownとchevronDownSmallは下向きの矢印です。globeは地球儀のアイコンです。
  *
  */
 export declare class SpIcon extends HTMLElement {
