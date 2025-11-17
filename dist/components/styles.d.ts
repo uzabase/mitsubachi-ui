bbde20631dd921c723987ef547cd2c35a399cbf4
Ittetsu Sato <ittetsusato@gmail.com>
2025-11-17 21:59:35 +0900

    chore: git rm -r --cached dist/

--- dist/components/styles.d.ts
+++
@@ -1,3 +0,0 @@
-import { CSSResult } from 'lit';
-export declare function makeStyles(...styles: CSSResult[]): CSSResult[];
-//# sourceMappingURL=styles.d.ts.map
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { SpButton } from './components/button/sp-button';
-export { SpIcon } from './components/icon';
-export { SpLabelUnit } from './components/label-unit';
-export { SpLogo } from './components/logo';
-export { SpControlMenu } from './components/menu/control-menu';
-export { SpControlMenuItem } from './components/menu/control-menu-item';
-export { SpTextFieldUnit } from './components/text-field/text-field-unit';
-//# sourceMappingURL=index.d.ts.map
