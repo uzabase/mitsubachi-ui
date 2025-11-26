0d92e11375dfeeef471b218c24053ebbcbccbebf
oki07 <takashi.oki@uzabase.com>
2025-11-27 01:59:03 +0900

    ラジオグループを削除

--- src/components/radio-button/sp-radio-button-text-group.ts
+++
@@ -1,9 +0,0 @@
-import { html, LitElement } from "lit";
-
-export class SpRadioButtonTextGroup extends LitElement {
-  render() {
-    return html`<div class="base">
-      <slot></slot>
-    </div>`;
-  }
-}
