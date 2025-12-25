4c93fa6f5ab4db904b5a9e2d8470a669ba7a17b0
oki07 <takashi.oki@uzabase.com>
2025-12-25 23:42:46 +0900

    floating-buttonを実装

--- src/components/floating-button/sp-floating-button.ts
+++ src/components/floating-button/sp-floating-button.ts
@@ -1,6 +1,8 @@
 import "../icon";
+import "../loading/sp-loading";
 
 import { html, LitElement } from "lit";
+import { property } from "lit/decorators.js";
 
 import { makeStyles } from "../styles";
 import styles from "./floating-button.styles";
@@ -8,9 +10,14 @@ import styles from "./floating-button.styles";
 export class SpFloatingButton extends LitElement {
   static styles = makeStyles(styles);
 
+  @property({ type: Boolean })
+  loading = false;
+
   render() {
     return html`<button class="base">
-      <sp-icon class="icon" type="magic-fill"></sp-icon>
+      ${this.loading
+        ? html`<sp-loading class="loading" ai size="2xLarge"></sp-loading>`
+        : html`<sp-icon class="icon" type="magic-fill"></sp-icon>`}
     </button>`;
   }
 }
