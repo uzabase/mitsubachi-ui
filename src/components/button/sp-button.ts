ed20164c054fdc959e345ba015e75c998c087902
Ittetsu Sato <ittetsusato@gmail.com>
2025-12-26 20:26:37 +0900

    fix: buttonのvariantsをdeprecatedにした

--- src/components/button/sp-button.ts
+++ src/components/button/sp-button.ts
@@ -55,6 +55,12 @@ export class SpButton extends LitElement {
   @property({ type: Boolean, reflect: true })
   danger = false;
 
+  /**
+   * @deprecated このプロパティは非推奨です。代わりに `variant` を使用してください。
+   */
+  @property({ type: String })
+  variants: Variant = "primary";
+
   @property({ type: String })
   variant: Variant = "primary";
 
@@ -83,6 +89,7 @@ export class SpButton extends LitElement {
     return [
       "base",
       this.danger ? "danger" : "normal",
+      isValidVariant(this.variants),
       isValidVariant(this.variant),
       sizeClassMap[isValidSize(this.size)],
       this.loading ? "loading" : "",
