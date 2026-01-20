a6f39c82bc01648baf83bef7ab69b14a24521ddf
GitHub Action <action@github.com>
2026-01-20 05:30:12 +0000

    distディレクトリを更新

--- dist/components/avatar/sp-avatar.d.ts
+++ dist/components/avatar/sp-avatar.d.ts
@@ -1,18 +1,20 @@
 import { LitElement } from 'lit';
-export declare const size: readonly ["small", "medium", "large"];
-type Size = (typeof size)[number];
+declare const size: readonly ["small", "medium", "large", "x-large", "2x-large"];
+export type AvatarSize = (typeof size)[number];
+declare const colors: readonly ["plum", "violet", "blue", "viridian", "green", "brown", "red"];
+export type AvatarColor = (typeof colors)[number];
 /**
  * @summary アバターです。
  */
 export declare class SpAvatar extends LitElement {
+    #private;
     static styles: import('lit').CSSResult[];
     src: string;
     alt: string;
-    name: string;
-    size: Size;
-    private get avatarClasses();
-    private get initials();
-    private handleImageError;
+    initials: string;
+    size: AvatarSize;
+    color: AvatarColor | "";
+    inactive: boolean;
     render(): import('lit-html').TemplateResult<1>;
 }
 declare global {
