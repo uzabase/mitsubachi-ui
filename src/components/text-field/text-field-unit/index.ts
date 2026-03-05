f5f4796e01d6fe6901381040c2c770ba6e5e92fe
Tsutomu YANO <tsutomu.yano@uzabase.com>
2026-03-05 17:30:47 +0900

    setFormValue

--- src/components/text-field/text-field-unit/index.ts
+++ src/components/text-field/text-field-unit/index.ts
@@ -73,6 +73,7 @@ export class MiTextFieldUnit extends LitElement {
   #handleInput(e: Event) {
     const target = e.target as MiTextField;
     this.value = target.value;
+    this.internals.setFormValue(target.value);
   }
 
   render() {
