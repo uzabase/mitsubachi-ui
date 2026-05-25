e2ebb660046204cc713f00af3e8e8a3859731dfa
Ryotaro Nakamura <ryotaro.nakamura@uzabase.com>
2026-05-25 16:03:33 +0900

    chore: update version to 2.4.1 and add api-extractor dependency
    - Updated package version from 2.4.1-alpha.0 to 2.4.1 in package.json and package-lock.json.
    - Added @microsoft/api-extractor as a dependency.
    - Changed the path for type definitions in package.json from dist/src/index.d.ts to dist/index.d.ts.
    - Updated build script to include a new step for generating type definitions.
    - Removed unnecessary dev dependencies related to @emnapi.

--- dist/mock.js
+++ dist/mock.js
@@ -47,6 +47,18 @@
   if (!customElements.get("sp-avatar")) {
     customElements.define("sp-avatar", class extends HTMLElement {});
   }
+  if (!customElements.get("mi-checkbox-text")) {
+    customElements.define("mi-checkbox-text", class extends HTMLElement {});
+  }
+  if (!customElements.get("sp-checkbox-text")) {
+    customElements.define("sp-checkbox-text", class extends HTMLElement {});
+  }
+  if (!customElements.get("mi-checkbox")) {
+    customElements.define("mi-checkbox", class extends HTMLElement {});
+  }
+  if (!customElements.get("sp-checkbox")) {
+    customElements.define("sp-checkbox", class extends HTMLElement {});
+  }
   if (!customElements.get("mi-action-dialog")) {
     customElements.define("mi-action-dialog", class extends HTMLElement {
       constructor() {
@@ -71,32 +83,20 @@
       }
     });
   }
-  if (!customElements.get("mi-checkbox-text")) {
-    customElements.define("mi-checkbox-text", class extends HTMLElement {});
-  }
-  if (!customElements.get("sp-checkbox-text")) {
-    customElements.define("sp-checkbox-text", class extends HTMLElement {});
-  }
-  if (!customElements.get("mi-checkbox")) {
-    customElements.define("mi-checkbox", class extends HTMLElement {});
-  }
-  if (!customElements.get("sp-checkbox")) {
-    customElements.define("sp-checkbox", class extends HTMLElement {});
-  }
   if (!customElements.get("mi-floating-button")) {
     customElements.define("mi-floating-button", class extends HTMLElement {});
   }
   if (!customElements.get("sp-floating-button")) {
     customElements.define("sp-floating-button", class extends HTMLElement {});
   }
+  if (!customElements.get("mi-icon-color")) {
+    customElements.define("mi-icon-color", class extends HTMLElement {});
+  }
   if (!customElements.get("mi-icon")) {
     customElements.define("mi-icon", class extends HTMLElement {});
   }
   if (!customElements.get("sp-icon")) {
     customElements.define("sp-icon", class extends HTMLElement {});
-  }
-  if (!customElements.get("mi-icon-color")) {
-    customElements.define("mi-icon-color", class extends HTMLElement {});
   }
   if (!customElements.get("mi-inline-notification")) {
     customElements.define("mi-inline-notification", class extends HTMLElement {
@@ -118,18 +118,6 @@
   if (!customElements.get("sp-loading")) {
     customElements.define("sp-loading", class extends HTMLElement {});
   }
-  if (!customElements.get("mi-logo")) {
-    customElements.define("mi-logo", class extends HTMLElement {});
-  }
-  if (!customElements.get("sp-logo")) {
-    customElements.define("sp-logo", class extends HTMLElement {});
-  }
-  if (!customElements.get("mi-speeda-logo")) {
-    customElements.define("mi-speeda-logo", class extends HTMLElement {});
-  }
-  if (!customElements.get("mi-uzabase-logo")) {
-    customElements.define("mi-uzabase-logo", class extends HTMLElement {});
-  }
   if (!customElements.get("mi-radio-button-text")) {
     customElements.define("mi-radio-button-text", class extends HTMLElement {
       constructor() {
@@ -145,6 +133,18 @@
         this.attachShadow({ mode: "open" }).innerHTML = "<slot></slot>";
       }
     });
+  }
+  if (!customElements.get("mi-logo")) {
+    customElements.define("mi-logo", class extends HTMLElement {});
+  }
+  if (!customElements.get("sp-logo")) {
+    customElements.define("sp-logo", class extends HTMLElement {});
+  }
+  if (!customElements.get("mi-speeda-logo")) {
+    customElements.define("mi-speeda-logo", class extends HTMLElement {});
+  }
+  if (!customElements.get("mi-uzabase-logo")) {
+    customElements.define("mi-uzabase-logo", class extends HTMLElement {});
   }
   if (!customElements.get("mi-snackbar-viewport")) {
     customElements.define("mi-snackbar-viewport", class extends HTMLElement {
@@ -170,6 +170,12 @@
       }
     });
   }
+  if (!customElements.get("mi-control-menu-item")) {
+    customElements.define("mi-control-menu-item", class extends HTMLElement {});
+  }
+  if (!customElements.get("sp-control-menu-item")) {
+    customElements.define("sp-control-menu-item", class extends HTMLElement {});
+  }
   if (!customElements.get("mi-control-menu")) {
     customElements.define("mi-control-menu", class extends HTMLElement {
       constructor() {
@@ -185,12 +191,6 @@
         this.attachShadow({ mode: "open" }).innerHTML = "<slot></slot>";
       }
     });
-  }
-  if (!customElements.get("mi-control-menu-item")) {
-    customElements.define("mi-control-menu-item", class extends HTMLElement {});
-  }
-  if (!customElements.get("sp-control-menu-item")) {
-    customElements.define("sp-control-menu-item", class extends HTMLElement {});
   }
   if (!customElements.get("mi-text-field")) {
     customElements.define("mi-text-field", class extends HTMLElement {});
